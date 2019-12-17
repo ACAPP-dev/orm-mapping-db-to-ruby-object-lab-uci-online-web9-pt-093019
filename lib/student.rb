980569569f668d4606cd61771841b5946d584b6d
Andrew Capp <acapp909@gmail.com>
2019-12-17 01:41:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,6 +61,12 @@ class Student
     end
   end
 
+  def self.first_student_in_grade_10
+    DB[:conn].execute("SELECT * FROM students WHERE grade = 10 LIMIT 1").map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
